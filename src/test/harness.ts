95c1ed7ae640b1e5a0f941d9616bb366d6ae64a3
Antony Rizzitelli <rizzitelli.antony@pm.me>
2026-09-24 07:28:25 +0200

    chore(deps): bump @antelopejs modules in the test harness to latest

--- src/test/harness.ts
+++ src/test/harness.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/dms",
-        version: ">=0.0.1 <1.0.0",
+        version: ">=0.4.0 <1.0.0",
       },
       config: {
         auth: {
@@ -44,7 +44,7 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/mongodb",
-        version: "1.3.0",
+        version: "1.3.1",
       },
     },
     "auth-jwt": {
@@ -61,14 +61,14 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/file-storage-local",
-        version: "0.1.4",
+        version: "0.1.5",
       },
     },
     nodemailer: {
       source: {
         type: "package",
         package: "@antelopejs/nodemailer",
-        version: "0.0.4",
+        version: "0.0.5",
       },
       config: {
         host: "127.0.0.1",
@@ -81,9 +81,10 @@ export default defineConfig({
       source: {
         type: "package",
         package: "@antelopejs/api",
-        version: "1.2.5",
+        version: "1.3.0",
       },
       config: {
+        publicBaseUrl: `http://127.0.0.1:${API_PORT}`,
         servers: [{ protocol: "http", host: "127.0.0.1", port: API_PORT }],
       },
     },
